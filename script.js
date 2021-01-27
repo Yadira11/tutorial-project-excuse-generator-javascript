61dfaabda537d16265c4f9dfc9fbf98d9c58beda
Yadira11 <chavarriayadira85@gmail.com>
2021-01-27 16:57:02 +0000

    Excuse-Generator-corrected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random() * 4)];
-let action = ['ate', 'peed', 'crushed', 'broke'][Math.floor(Math.random() * 3)];
-let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random() * 3)];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random() * 5)];
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random(who.length) )];
+let action = ['ate', 'peed', 'crushed', 'broke'][Math.floor(Math.random(who.length) )];
+let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random(who.length) )];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random(who.length) )];
 
 document.querySelector("#excuse").innerHTML =who + " "+action + " "+what + " "+when;
