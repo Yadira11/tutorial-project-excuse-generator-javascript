4d514fa7e95edae4d1ad98ee0ebbf8ddfe109cac
Yadira11 <chavarriayadira85@gmail.com>
2021-02-26 00:57:50 +0000

    finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][Math.floor(Math.random(who.length) )];
-let action = ['ate', 'peed', 'crushed', 'broke'][Math.floor(Math.random(who.length) )];
-let what = ['my homework', 'the keys', 'the car'][Math.floor(Math.random(who.length) )];
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][Math.floor(Math.random(who.length) )];
+const arrayLength = Math.floor(Math.random(who.length));
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'][arrayLength];
+let action = ['ate', 'peed', 'crushed', 'broke'][arrayLength];
+let what = ['my homework', 'the keys', 'the car'][arrayLength];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'][arrayLength];
 
-document.querySelector("#excuse").innerHTML =who + " "+action + " "+what + " "+when;
+document.querySelector("#excuse").innerHTML = who + " " + action + " " + what + " " + when;
